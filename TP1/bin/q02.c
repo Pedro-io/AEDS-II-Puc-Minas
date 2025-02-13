fe424c79ad88627534968b0f3aebbc3f029e3958
Pedro-io <mayper2532@gmail.com>
2025-02-13 20:49:40 -0300

    inicio q02

--- TP1/bin/q02.c
+++ TP1/bin/q02.c
@@ -6,7 +6,27 @@ int main()
 
     scanf("%s", palavra);
     
-    
+    while(palavra != "FIM")
+    {
+
+    }
 
     return 0;
+}
+
+char PalavraReversa(char *palavra) {
+    /* 
+            função para reverter a palavra
+            parametros: 
+            String palavra: string de entrada do usuário que vai ser revertida
+
+            return : String com o reverso da entrada
+       */     
+
+    char palavraReversa[100] = ""; // declara uma variavel String vazia para inserir a palavra reversa 
+    for (int i = palavra - 1 ; i >= 0; i--) { // para cada caracter da palavra de tamanho n 
+        palavraReversa += palavra.charAt(i);// insira na posição n(i) da palavra reversa  
+    }
+
+    return palavraReversa;// retorna a palavra 
 }

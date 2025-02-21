ce4e92b258f9a7cc19725e4cd8cef0a5b73864d1
Pedro-io <p.rodrigues2532@gmail.com>
2025-02-21 12:13:47 -0300

    q02 resolvida

--- TP1/src/q02.c
+++ TP1/src/q02.c
@@ -1,84 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int length(const char *str) {
-    int length = 0;
-    while (str[length] != '\0') {
-      length++;
-    }
-    return length;
+int length(const char *str)
+{ // Pega o tamanho do arquivo
+  int length = 0;
+  while (str[length] != '\0')
+  {
+    length++;
   }
+  return length;
+}
 
-char* PalavraReversa(const char *palavra, int tamanho){
-  
-  char *rev = (char*)malloc((tamanho + 1) * sizeof(char));
+char *PalavraReversa(const char *palavra, int tamanho)
+{
+  // inverte a palavra
+  char *rev = (char *)malloc((tamanho + 1) * sizeof(char));
 
+  for (int i = tamanho - 1, j = 0; i >= 0; i--, j++)
+  {
 
-  for(int i = tamanho - 1, j = 0; i >= 0; i--, j++){
-        
     rev[j] = palavra[i];
   }
 
-  rev[tamanho] = '\0'; 
+  rev[tamanho] = '\0';
 
   return rev;
 }
 
-
-const char* VerificaString(const char* palavra, const char* palavraReversa, int tamanho) {
-  /* Função para verificar se é um palindrono ou não 
-   * parametros: 
-   *  String palavra: palavra inserida pelo o usuario 
-   *  String palavraReversa: o reverso da palavra inserida pelo usuario 
-   * 
+const char *verificaString(const char *palavra, const char *palavraReversa, int tamanho)
+{
+  /* Função para verificar se é um palindrono ou não
+   * parametros:
+   *  String palavra: palavra inserida pelo o usuario
+   *  String palavraReversa: o reverso da palavra inserida pelo usuario
+   *
    * return : "SIM" ou "NÃO"
    */
 
-  for (int i = 0; i < tamanho; i++){
-      if (palavra[i] != palavraReversa[i]) {
-          return "NAO"; // Se encontrar uma diferença, não é palíndromo
-      }
+  for (int i = 0; i < tamanho; i++)
+  {
+    if (palavra[i] != palavraReversa[i])
+    {
+      return "NAO"; // Se encontrar uma diferença, não é palíndromo
+    }
   }
 
   return "SIM"; // Se chegou até aqui, todos os caracteres corresponderam, é palíndromo
-
-
 }
 
-int EhFIM(const char *palavra, int tamanho) {
+int EhFIM(const char *palavra, int tamanho)
+{
   return (tamanho == 3 && palavra[0] == 'F' && palavra[1] == 'I' && palavra[2] == 'M');
 }
 
-
-
 int main()
 {
-    char palavra[200]; 
-    
-    scanf("%[^\n]", palavra);
-    getchar();
+  char palavra[2000];
 
-    int tamanho = length(palavra);
+  scanf("%[^\n]", palavra);
+  getchar();
 
-    char* palavraReversa = PalavraReversa(palavra, tamanho);
-    
-    
-    while (!EhFIM(palavra, tamanho))
-    {
-      
-      printf("%s\n", VerificaString(palavra, palavraReversa, tamanho));
+  int tamanho = length(palavra);
+  char *palavraReversa = PalavraReversa(palavra, tamanho);
 
-      scanf("%[^\n]", palavra);
-      getchar();
-      tamanho = length(palavra);
+  while (!EhFIM(palavra, tamanho))
+  {
 
-
-      palavraReversa = PalavraReversa(palavra, tamanho);
-    }
-
+    printf("%s\n", verificaString(palavra, palavraReversa, tamanho));
     free(palavraReversa);
-    
+    scanf(" %[^\n]", palavra);
+    tamanho = length(palavra);
+    palavraReversa = PalavraReversa(palavra, tamanho);
+  }
 
-    return 0;
+ 
+
+  return 0;
 }
-
--- TP1/src/q03.java
+++ TP1/src/q03.java
@@ -1,42 +1,27 @@
 import java.util.*;
 
-
 public class q03 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        
-        String entrada = sc.nextLine(); 
+        String entrada = sc.nextLine();
 
         String resp = Criptografa(entrada);
 
-        System.out.println(resp); 
+        System.out.println(resp);
 
         sc.close();
 
     }
 
+    public static String Criptografa(String entrada) {
 
-    public static String Criptografa(String entrada){
+        String resp = "";
+
         
-        String resp ="";
-        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                            "abcdefghijklmnopqrstuvwxyz" +
-                            "ÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇ" +
-                           "áàãâäéèêëíìîïóòõôöúùûüç";
-        for(int i = 0; i <= entrada.length(); i++){
-            for(int j = 0; j <= letras.length(); j++){
 
-                if(entrada.charAt(i) == letras.charAt(j)){
-                    resp += letras.charAt(j + 3);
-                }
+        return resp; 
 
-            }
-            
-        }
-
-
-        return resp;
     }
 }
